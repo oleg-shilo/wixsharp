7dbe6ecbddf48d242b713fbbe61799fa85658061
oleg-shilo <oleg.shilo@gmail.com>
2016-12-09 13:41:08 +1100

    Release v1.2 Issue#1: Compiler.BuildMsi() & async/await Issue#2: Cannot add environment variables to project object if only merge modules are present. Implemented burn compiler output to catch WixMbaPrereqVars conflicts and suggest the work around. (Related to CP issue#149)

--- Source/src/WixSharp.Samples/Wix# Samples/MergeModules/Consuming MergeModule/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/MergeModules/Consuming MergeModule/setup.cs
@@ -13,28 +13,29 @@ using System.Text.RegularExpressions;
 
 class Script
 {
-static public void Main(string[] args)
-{
-    var featureA = new Feature("Feature A", "Feature A description");
-    var featureB = new Feature("Feature B", "Feature B description");
-    var complete = new Feature("Complete");
+    static public void Main(string[] args)
+    {
+        var featureA = new Feature("Feature A", "Feature A description");
+        var featureB = new Feature("Feature B", "Feature B description");
+        var complete = new Feature("Complete");
 
-    complete.Add(featureA)
-            .Add(featureB);
+        complete.Add(featureA)
+                .Add(featureB);
 
-    var project =
-            new Project("MyMergeModuleSetup",
-                new Dir(@"%ProgramFiles%\My Company",
-                    new File(featureA, @"Files\MainFile.txt"),
-                    new Merge(featureB, @"Files\MyMergeModule.msm"),
-                    new Merge(featureB, @"Files\MyMergeModule1.msm")));
+        var project =
+                new Project("MyMergeModuleSetup",
+                    new Dir(@"%ProgramFiles%\My Company",
+                        //new File(featureA, @"Files\MainFile.txt"),
+                        new Merge(featureB, @"Files\MyMergeModule.msm"),
+                        new Merge(featureB, @"Files\MyMergeModule1.msm"))
+                        , new EnvironmentVariable("foo", "bar"));
 
-    project.DefaultFeature = complete;
-    project.UI = WUI.WixUI_FeatureTree;
-    project.InstallerVersion = 200; //you may want to change it to match MSM module installer version
+        project.DefaultFeature = complete;
+        project.UI = WUI.WixUI_FeatureTree;
+        project.InstallerVersion = 200; //you may want to change it to match MSM module installer version
 
-    project.PreserveTempFiles = true;
+        project.PreserveTempFiles = true;
 
-    project.BuildMsi();
-}
+        project.BuildMsi();
+    }
 }
