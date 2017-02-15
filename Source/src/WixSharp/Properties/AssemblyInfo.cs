40372ac8281534734d7be70029825bc046adad40
oleg-shilo <oleg.shilo@gmail.com>
2017-02-15 14:02:43 +1100

    # Release v1.4.0 - Issue #14: Support for InternetShortcut element - Issue #10:*.g.wxs not created in TeamCity build - Added MSBuild.EmitAutoGenFiles

--- Source/src/WixSharp/Properties/AssemblyInfo.cs
+++ Source/src/WixSharp/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
