2414fe76bd195f884f754484fef2c802dd2ecee5
oleg-shilo <oleg.shilo@gmail.com>
2017-04-23 18:38:56 +1000

    Release v1.4.3.0 * Added support for multiple absolute paths. Part of Issue #55 effort. * Issue #55: AbsolutePath support is broken * Issue #48: WixSharp.Files generation: same folder on different features. * Issue #45: Can't install dll to windows/system32 * issues#43: How to mark a .NET assembly to be NGen'd during install * Generic items (`IGenericEntity`) are now allowed in `File` and `Assembly` constructor. * Implemented N-to-N relationship between Wix object/components and `Feature`s. Triggered by issue#48. * [Bootstrapper]support exit code feature * Project Templates extension is migrated to VS2017

--- Source/src/WixSharp/Properties/AssemblyInfo.cs
+++ Source/src/WixSharp/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.2.1")]
-[assembly: AssemblyFileVersion("1.4.2.1")]
+[assembly: AssemblyVersion("1.4.3.0")]
+[assembly: AssemblyFileVersion("1.4.3.0")]
