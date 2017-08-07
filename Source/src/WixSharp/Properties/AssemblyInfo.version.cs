94c0d6e385f8d81940ed999360465787174e1971
oleg-shilo <oleg.shilo@gmail.com>
2017-08-07 11:53:07 +1000

    Nuget pre-release v1.4.7.1 - Issue #116: DigitalySign with Keystore

--- Source/src/WixSharp/Properties/AssemblyInfo.version.cs
+++ Source/src/WixSharp/Properties/AssemblyInfo.version.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.7.0")]
-[assembly: AssemblyFileVersion("1.4.7.0")]
+[assembly: AssemblyVersion("1.4.7.1")]
+[assembly: AssemblyFileVersion("1.4.7.1")]
