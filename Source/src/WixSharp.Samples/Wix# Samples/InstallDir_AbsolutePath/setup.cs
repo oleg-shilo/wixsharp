1703b388483be7dc079cab7177ce438c91a92c08
oleg-shilo <oleg.shilo@gmail.com>
2018-01-23 21:56:30 +1100

    - Samples update.

--- Source/src/WixSharp.Samples/Wix# Samples/InstallDir_AbsolutePath/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/InstallDir_AbsolutePath/setup.cs
@@ -3,6 +3,7 @@
 //css_ref System.Core.dll;
 //css_ref WixSharp.UI.dll;
 using System;
+using System.Windows.Forms;
 using WixSharp;
 
 class Script
@@ -10,13 +11,22 @@ class Script
     static public void Main()
     {
         var project = new ManagedProject("MyProduct",
-                          new Dir(@"C:\MyCompany2", new File("setup.cs")),
+                          new Dir(@"C:\MyCompany2",
+                              new File("setup.cs")),
                           new Dir(@"C:\MyCompany\MyProduct",
                               new Files(@"files\*.*")));
 
         project.UI = WUI.WixUI_ProgressOnly;
+        project.AfterInstall += Project_AfterInstall;
 
         Compiler.PreserveTempFiles = true;
         Compiler.BuildMsi(project);
+    }
+
+    static void Project_AfterInstall(SetupEventArgs e)
+    {
+        MessageBox.Show("e.InstallDir -> " + e.InstallDir + "\n" +
+                        "EnvVar('INSTALLDIR') -> " + Environment.GetEnvironmentVariable("INSTALLDIR"),
+                        "AfterInstall");
     }
 }
