96666dac4dc458dc7950100e798eb18a05caf2dc
oleg-shilo <oleg.shilo@gmail.com>
2018-01-24 16:17:42 +1100

    - Removed old test code

--- Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
@@ -16,7 +16,6 @@ class Script
     {
         try
         {
-            File main_file;
             var project =
                 new Project("My Product",
                     new Dir(@"%ProgramFiles%\My Company\My Product",
@@ -27,39 +26,25 @@ class Script
                         },
                         new Dir("Samples",
                             new File(@"AppFiles\MyApp.cs")),
-                        main_file = new File(@"AppFiles\MyApp.exe",
+
+                        new File(@"AppFiles\MyApp.exe",
                             new FileShortcut("MyApp", "INSTALLDIR"), //INSTALLDIR is the ID of "%ProgramFiles%\My Company\My Product"
-
-                            // new FileShortcut("MyApp2", "INSTALLDIR") { IconFile = @"AppFiles\Icon.ico", WorkingDirectory = "%Temp%", Arguments = "333" },
-
                             new FileShortcut("MyApp", @"%Desktop%") { IconFile = @"AppFiles\Icon.ico", WorkingDirectory = "%Temp%", Arguments = "777" }),
                         new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]")
                         {
                             WorkingDirectory = "%Temp%"
-                        }
-                        ),
+                        }),
 
                     new Dir(@"%ProgramMenu%\My Company\My Product",
                         new ExeFileShortcut("Samples", @"[" + @"%ProgramFiles%\My Company\My Product\Samples".ToDirID() + "]", ""),
                         new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]")));
-
-            project.ResolveWildCards(ignoreEmptyDirectories: true)
-                   .FindFile(f => f.Name.EndsWith("MyApp.exe", StringComparison.OrdinalIgnoreCase))
-                   .First()
-                   .AddShortcut(
-                       new FileShortcut("MyApp_AnotherOnle", "INSTALLDIR")
-                       {
-                           IconFile = @"AppFiles\Icon.ico",
-                           WorkingDirectory = "%Temp%",
-                           Arguments = "777"
-                       });
 
             project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
             project.UI = WUI.WixUI_ProgressOnly;
             project.OutFileName = "setup";
             project.PreserveTempFiles = true;
 
-            Compiler.BuildMsiCmd(project);
+            Compiler.BuildMsi(project);
         }
         catch (System.Exception ex)
         {
