90f8ed1d69a9fab3afb8cb50eb7a70e3aef62469
oleg-shilo <oleg.shilo@gmail.com>
2018-01-24 16:03:53 +1100

    - Improved API documentation to address mistakes as in Issue #269 (File.AddShortcut Extension has strange side effects)

--- Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
@@ -7,6 +7,8 @@ using System.Xml;
 using Microsoft.Win32;
 using System.Windows.Forms;
 using WixSharp;
+using System.Linq;
+using WixSharp.CommonTasks;
 
 class Script
 {
@@ -14,6 +16,7 @@ class Script
     {
         try
         {
+            File main_file;
             var project =
                 new Project("My Product",
                     new Dir(@"%ProgramFiles%\My Company\My Product",
@@ -24,8 +27,11 @@ class Script
                         },
                         new Dir("Samples",
                             new File(@"AppFiles\MyApp.cs")),
-                        new File(@"AppFiles\MyApp.exe",
+                        main_file = new File(@"AppFiles\MyApp.exe",
                             new FileShortcut("MyApp", "INSTALLDIR"), //INSTALLDIR is the ID of "%ProgramFiles%\My Company\My Product"
+
+                            // new FileShortcut("MyApp2", "INSTALLDIR") { IconFile = @"AppFiles\Icon.ico", WorkingDirectory = "%Temp%", Arguments = "333" },
+
                             new FileShortcut("MyApp", @"%Desktop%") { IconFile = @"AppFiles\Icon.ico", WorkingDirectory = "%Temp%", Arguments = "777" }),
                         new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]")
                         {
@@ -37,12 +43,23 @@ class Script
                         new ExeFileShortcut("Samples", @"[" + @"%ProgramFiles%\My Company\My Product\Samples".ToDirID() + "]", ""),
                         new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]")));
 
+            project.ResolveWildCards(ignoreEmptyDirectories: true)
+                   .FindFile(f => f.Name.EndsWith("MyApp.exe", StringComparison.OrdinalIgnoreCase))
+                   .First()
+                   .AddShortcut(
+                       new FileShortcut("MyApp_AnotherOnle", "INSTALLDIR")
+                       {
+                           IconFile = @"AppFiles\Icon.ico",
+                           WorkingDirectory = "%Temp%",
+                           Arguments = "777"
+                       });
+
             project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
             project.UI = WUI.WixUI_ProgressOnly;
             project.OutFileName = "setup";
             project.PreserveTempFiles = true;
 
-            Compiler.BuildMsi(project);
+            Compiler.BuildMsiCmd(project);
         }
         catch (System.Exception ex)
         {
--- Source/src/WixSharp/FileShortcut.cs
+++ Source/src/WixSharp/FileShortcut.cs
@@ -1,10 +1,11 @@
 #region Licence...
+
 /*
 The MIT License (MIT)
 
 Copyright (c) 2014 Oleg Shilo
 
-Permission is hereby granted, 
+Permission is hereby granted,
 free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
@@ -23,39 +24,42 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-#endregion
+
+#endregion Licence...
+
 using System;
+
 namespace WixSharp
 {
     /// <summary>
     /// Defines <see cref="FileShortcut"/> to be installed.
     /// <para>
-    /// <see cref="FileShortcut"/> is a specialized version of the <see cref="Shortcut"/> designed 
+    /// <see cref="FileShortcut"/> is a specialized version of the <see cref="Shortcut"/> designed
     /// for using <see cref="Shortcut"/> as a <see cref="File"/> nested element.
     /// </para>
     /// <para>There are different ways of defining shortcuts of the Wix# project: </para>
-    /// <para> - It can be specified as a nested element of the <see cref="File"/>. In this case, after 
+    /// <para> - It can be specified as a nested element of the <see cref="File"/>. In this case, after
     /// the installation, the shortcut will point to the file it belongs to.</para>
-    /// <para> - Alternatively the <c>Shortcut</c> can be specified as a <see cref="File"/> nested 
-    /// element. In this case after the installation the shortcut will point to the file 
+    /// <para> - Alternatively the <c>Shortcut</c> can be specified as a <see cref="File"/> nested
+    /// element. In this case after the installation the shortcut will point to the file
     /// will point to the file it belongs to.</para>
     /// </summary>
-    /// 
+    ///
     /// <example>
-    /// The following is an example of installing <c>MyApp.exe</c> file with the corresponding 
+    /// The following is an example of installing <c>MyApp.exe</c> file with the corresponding
     /// shortcuts in on <c>Desktop</c> and <c>Program Menu</c>.
     /// <code>
-    /// var project = 
+    /// var project =
     ///     new Project("My Product",
-    ///     
+    ///
     ///         new Dir(@"%ProgramFiles%\My Company\My Product",
-    ///         
+    ///
     ///             new File(binaries, @"AppFiles\MyApp.exe",
     ///                 new FileShortcut("MyApp", @"%ProgramMenu%\My Company\My Product"),
     ///                 new FileShortcut("MyApp", @"%Desktop%")),
-    ///                 
+    ///
     ///         ...
-    ///         
+    ///
     /// Compiler.BuildMsi(project);
     /// </code>
     /// </example>
@@ -81,6 +85,7 @@ namespace WixSharp
             Feature = feature;
             Location = location;
         }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FileShortcut"/> class with properties/fields initialized with specified parameters.
         /// <para>This constructor should be used to instantiate shortcuts, which belong to the <see cref="File"/> element.</para>
@@ -92,12 +97,17 @@ namespace WixSharp
             Feature = feature;
             Location = location;
         }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FileShortcut"/> class with properties/fields initialized with specified parameters.
         /// <para>This constructor should be used to instantiate shortcuts, which belong to the <see cref="File"/> element.</para>
         /// </summary>
         /// <param name="name">The name of the shortcut to be installed.</param>
-        /// <param name="location">The directory where the shortcut should be installed.</param>
+        /// <param name="location">The ID of the directory where the shortcut should be installed.
+        /// <para>
+        /// Note it is not a path expression but an ID (e.g. "INSTALLDIR").
+        /// </para>
+        /// </param>
         public FileShortcut(string name, string location)
         {
             Name = name;
@@ -112,7 +122,6 @@ namespace WixSharp
             set { throw new ApplicationException("Despite the fact that " + this.GetType() + " has member Condition you should not use as the WiX Shortcut element doesn't support Condition."); }
         }
 
-
         /// <summary>
         /// Initializes a new instance of the <see cref="FileShortcut"/> class with properties/fields initialized with specified parameters.
         /// <para>This constructor should be used to instantiate shortcuts, which belong to the <see cref="File"/> element.</para>
@@ -126,6 +135,7 @@ namespace WixSharp
             Name = name;
             Location = location;
         }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FileShortcut"/> class with properties/fields initialized with specified parameters.
         /// <para>This constructor should be used to instantiate shortcuts, which belong to the <see cref="File"/> element.</para>
@@ -138,6 +148,7 @@ namespace WixSharp
         {
             Name = name;
         }
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FileShortcut"/> class with properties/fields initialized with specified parameters.
         /// <para>This constructor should be used to instantiate shortcuts, which belong to the <see cref="File"/> element.</para>
