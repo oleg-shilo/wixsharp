52cd462598ec3cb383457e6ccfd3b7bd93185399
Matt Slagle <Matt.Slagle@onbase.com>
2018-07-13 13:59:11 -0400

    Added an argument utilities to parse arguments easier Modified locating logic for win package files.  Logic now searches in this order 1. Set location in installer project via Compiler.WixLocation  a. If location is invalid, keep searching 2. Referenced package 3. Environment variable 4. Wix install

---
+++ Source/src/WixSharp/Utilities/ArgumentUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WixSharp.Utilities
+{
+    static class ArgumentUtilities
+    {
+        public static string GetArgumentValue(string[] possiblePreffixes)
+        {
+            foreach (var preffix in possiblePreffixes)
+            {
+                string arg = Environment.GetCommandLineArgs().Where(x => x.StartsWith(preffix, true)).LastOrDefault() ?? "";
+
+                if (arg.IsNotEmpty())
+                {
+                    return arg.Substring(preffix.Length);
+                }
+            }
+
+            return string.Empty;
+        }
+    }
+}
