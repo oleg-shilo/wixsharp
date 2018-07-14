b7c93b100f9bed1ae5e33f14bd63f50cc02060a8
oleg-shilo <oleg.shilo@gmail.com>
2018-07-14 19:19:40 +1000

    Added comment reflecting issue #416 in the corresponding sample.

--- Source/src/WixSharp.Samples/Wix# Samples/Bootstrapper/WixBootstrapper_UI/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Bootstrapper/WixBootstrapper_UI/setup.cs
@@ -28,9 +28,14 @@ public class Script
 
         bootstrapper.Version = new Version("1.0.0.0");
         bootstrapper.UpgradeCode = new Guid("6f330b47-2577-43ad-9095-1861bb25889a");
-        
+
         // You can also use System.Reflection.Assembly.GetExecutingAssembly().Location instead of "%this%"
-        // Note, passing "BootstrapperCore.config" is optional and provided for demo purposes only
+        // Note, passing BootstrapperCore.config is optional and if not provided the default BootstrapperCore.config
+        // will be used. The content of the default BootstrapperCore.config can be accessed via 
+        // ManagedBootstrapperApplication.DefaultBootstrapperCoreConfigContent.
+        //
+        // Note that the DefaultBootstrapperCoreConfigContent may not be suitable for all build and runtime scenarios.
+        // In such cases you may need to use custom BootstrapperCore.config as demonstrated below.
         bootstrapper.Application = new ManagedBootstrapperApplication("%this%", "BootstrapperCore.config"); 
 
         bootstrapper.PreserveTempFiles = true;
