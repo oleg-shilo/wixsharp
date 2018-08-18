500a08c400a8bf837e7a8c5b60eb4fdf90794f19
tigran <tigran@anchorfree.com>
2018-08-17 19:55:40 -0700

    Allows to repair the MSI package even when the original installation package is no longer available.
    Adds an additional source of the resiliency by attempting to create a symbolic link to the locally cached MSI package (%WINDIR%\Installer) or a hard link/full copy to the original installation MSI package in the specified directory.
    
    WIXSHARP_RESILIENT_SOURCE_DIR property can be used to configure the target directory. INSTALLDIR property is used by default.
    
    Windows 7 is shipped with the Windows Installer version 5.0, which unlike the previous versions of the windows installer caches the entire MSI, including internal CAB files. Unfortunately the complete cached MSI package is not used for repairs, a call is made to the original source which might not be available.
    
    See also:
    
    https://docs.microsoft.com/en-us/windows/desktop/msi/source-resiliency
    https://www.symantec.com/connect/articles/reducing-windows-installer-disk-wastage-windows-7

---
+++ Source/src/WixSharp/Utilities/RegistryWOW6432.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+// http://www.rhyous.com/2011/01/24/how-read-the-64-bit-registry-from-a-32-bit-application-or-vice-versa/
+// https://stackoverflow.com/questions/26217199/what-are-some-alternatives-to-registrykey-openbasekey-in-net-3-5
+namespace WixSharp.Utilities
+{
+    #region Enums
+    enum RegSAM
+    {
+        QueryValue = 0x0001,
+        SetValue = 0x0002,
+        CreateSubKey = 0x0004,
+        EnumerateSubKeys = 0x0008,
+        Notify = 0x0010,
+        CreateLink = 0x0020,
+        WOW64_32Key = 0x0200,
+        WOW64_64Key = 0x0100,
+        WOW64_Res = 0x0300,
+        Read = 0x00020019,
+        Write = 0x00020006,
+        Execute = 0x00020019,
+        AllAccess = 0x000f003f
+    }
+    #endregion
+
+    static class RegHive
+    {
+        public static UIntPtr HKEY_LOCAL_MACHINE = new UIntPtr(0x80000002u);
+        public static UIntPtr HKEY_CURRENT_USER = new UIntPtr(0x80000001u);
+    }
+
+    static class RegistryWOW6432
+    {
+        #region Member Variables
+        #region Read 64bit Reg from 32bit app
+        [DllImport("Advapi32.dll")]
+        static extern uint RegOpenKeyEx(
+            UIntPtr hKey,
+            string lpSubKey,
+            uint ulOptions,
+            int samDesired,
+            out int phkResult);
+
+        [DllImport("Advapi32.dll")]
+        static extern uint RegCloseKey(int hKey);
+
+        [DllImport("advapi32.dll", EntryPoint = "RegQueryValueEx")]
+        static extern int RegQueryValueEx(
+            int hKey, string lpValueName,
+            int lpReserved,
+            ref uint lpType,
+            StringBuilder lpData,
+            ref uint lpcbData);
+        #endregion
+        #endregion
+
+        #region Functions
+        public static string GetRegKey64(UIntPtr inHive, String inKeyName, String inPropertyName)
+        {
+            return GetRegKey64(inHive, inKeyName, RegSAM.WOW64_64Key, inPropertyName);
+        }
+
+        public static string GetRegKey32(UIntPtr inHive, String inKeyName, String inPropertyName)
+        {
+            return GetRegKey64(inHive, inKeyName, RegSAM.WOW64_32Key, inPropertyName);
+        }
+
+        public static string GetRegKey64(UIntPtr inHive, String inKeyName, RegSAM in32or64key, String inPropertyName)
+        {
+            int hkey = 0;
+
+            try
+            {
+                uint lResult = RegOpenKeyEx(RegHive.HKEY_LOCAL_MACHINE, inKeyName, 0, (int)RegSAM.QueryValue | (int)in32or64key, out hkey);
+                if (0 != lResult) return null;
+                uint lpType = 0;
+                uint lpcbData = 1024;
+                StringBuilder AgeBuffer = new StringBuilder(1024);
+                RegQueryValueEx(hkey, inPropertyName, 0, ref lpType, AgeBuffer, ref lpcbData);
+                string Age = AgeBuffer.ToString();
+                return Age;
+            }
+            finally
+            {
+                if (0 != hkey) RegCloseKey(hkey);
+            }
+        }
+        #endregion
+    }
+}
