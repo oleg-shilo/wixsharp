fd59c58c9caec0bf49dcb29a4062d35b02a67264
oleg-shilo <oleg.shilo@gmail.com>
2018-09-21 21:25:06 +1000

    - Bootstrapper_UI sample is updated to show how to pass user input from BA to MSI package.

--- Source/src/WixSharp.Samples/Wix# Samples/Bootstrapper/WixBootstrapper_UI/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Bootstrapper/WixBootstrapper_UI/setup.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Windows;
 using WixSharp;
 using WixSharp.Bootstrapper;
 using io = System.IO;
@@ -11,10 +12,15 @@ public class Script
     static public void Main(string[] args)
     {
         var productProj =
-            new Project("My Product",
+            new ManagedProject("My Product",
                 new Dir(@"%ProgramFiles%\My Company\My Product",
                     new File("readme.txt")))
             { InstallScope = InstallScope.perMachine };
+
+        productProj.Load += (SetupEventArgs e)=>
+        {
+            MessageBox.Show(e.Session.Property("USERINPUT"));
+        };
 
         productProj.GUID = new Guid("6f330b47-2577-43ad-9095-1861bb258777");
         string productMsi = productProj.BuildMsi();
@@ -24,8 +30,14 @@ public class Script
         var bootstrapper =
                 new Bundle("My Product",
                     new PackageGroupRef("NetFx40Web"),
-                    new MsiPackage(productMsi) { Id = "MyProductPackageId", DisplayInternalUI = true });
+                    new MsiPackage(productMsi)
+                    {
+                        Id = "MyProductPackageId",
+                        DisplayInternalUI = true,
+                        MsiProperties = "USERINPUT=[UserInput]"
+                    });
 
+        bootstrapper.Variables = new[] { new Variable("UserInput", "<none>"), };
         bootstrapper.Version = new Version("1.0.0.0");
         bootstrapper.UpgradeCode = new Guid("6f330b47-2577-43ad-9095-1861bb25889a");
 
@@ -36,7 +48,7 @@ public class Script
         //
         // Note that the DefaultBootstrapperCoreConfigContent may not be suitable for all build and runtime scenarios.
         // In such cases you may need to use custom BootstrapperCore.config as demonstrated below.
-        bootstrapper.Application = new ManagedBootstrapperApplication("%this%", "BootstrapperCore.config"); 
+        bootstrapper.Application = new ManagedBootstrapperApplication("%this%", "BootstrapperCore.config");
 
         bootstrapper.PreserveTempFiles = true;
         bootstrapper.SuppressWixMbaPrereqVars = true;
@@ -44,4 +56,6 @@ public class Script
         bootstrapper.Build("my_app.exe");
         io.File.Delete(productMsi);
     }
+
+    
 }
