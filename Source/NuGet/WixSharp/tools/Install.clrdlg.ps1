13bde13d2ae14d5b5ef804582591eca903eecea1
Oleg Shilo <oleg.shilo@leicabyosystems.com>
2019-04-03 23:23:29 +1100

    - Completely removed all traces of `SetEnvVar.dll`. Part of #620 fix.

--- Source/NuGet/WixSharp/tools/Install.clrdlg.ps1
+++ Source/NuGet/WixSharp/tools/Install.clrdlg.ps1
@@ -7,31 +7,11 @@ $debugString = "install.ps1 executing for " + $projectFullName
 $project.Save()
 
 #Load the csproj file into an xml object
-$xml = [XML] (gc $project.FullName)
+$xml = [XML] (Get-Content $project.FullName)
 
 #grab the namespace from the project element so your xpath works.
 $nsmgr = New-Object System.Xml.XmlNamespaceManager -ArgumentList $xml.NameTable
-$nsmgr.AddNamespace('a',$xml.Project.GetAttribute("xmlns"))
-
-#link the custom target
-#<UsingTask AssemblyFile="packages\WixSharp.1.0.20.0\build\SetEnvVar.dll" TaskName="SetEnvVar" />
-$node = $xml.Project.SelectSingleNode("//a:UsingTask[@TaskName='SetEnvVar']", $nsmgr)
-if($node -ne $null)
-{
-    $xml.Project.RemoveChild($node)
-}
-
-$usingTask = $xml.CreateElement("UsingTask", $xml.Project.GetAttribute("xmlns"))
-
-$attr = $xml.CreateAttribute("AssemblyFile")
-$attr.Value = '$(SolutionDir)'+"packages\"+$package.Id+"."+$package.Version+"\build\SetEnvVar.dll"
-$usingTask.Attributes.Append($attr)
-
-$attr = $xml.CreateAttribute("TaskName")
-$attr.Value = "SetEnvVar"
-$usingTask.Attributes.Append($attr)
-
-$xml.Project.AppendChild($usingTask)
+$nsmgr.AddNamespace('a', $xml.Project.GetAttribute("xmlns"))
 
 #link the form designer to the EmptyDialog.cs
 $node = $xml.Project.SelectSingleNode("//a:Compile[@Include='EmptyDialog.Designer.cs']", $nsmgr)
@@ -47,8 +27,7 @@ $node.AppendChild($depUpon)
 
 #delete the bundled program.cs file.
 $prog = $xml.Project.SelectSingleNode("//a:Compile[@Include='Program.cs']", $nsmgr)
-if($prog -ne $null)
-{
+if ($prog -ne $null) {
     $prog.SelectSingleNode("..").RemoveChild($prog)
 }
 #save the changes.
