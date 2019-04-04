b5dfab40d35acc4c6f87dbb933fbab070571f1e1
Oleg Shilo <oleg.shilo@leicabyosystems.com>
2019-04-04 17:41:10 +1100

    - WixSharp.VIX v1.8.0.0   * Project templetes now include all WixSharp references to work smoothly on NuGet restore   * Removed SetEnvVar.dll target from the project templete

--- Source/Templates/ProjectTemplates/WixSharp Managed Setup - Custom Dialog/CustomDialog.cs
+++ Source/Templates/ProjectTemplates/WixSharp Managed Setup - Custom Dialog/CustomDialog.cs
@@ -10,14 +10,14 @@ namespace WixSharpSetup
         public CustomDialog()
         {
             //NOTE: If this assembly is compiled for v4.0.30319 runtime, it may not be compatible with the MSI hosted CLR.
-            //The incompatibility is particularly possible for the Embedded UI scenarios. 
+            //The incompatibility is particularly possible for the Embedded UI scenarios.
             //The safest way to avoid the problem is to compile the assembly for v3.5 Target Framework.$safeprojectname$
             InitializeComponent();
         }
 
         void dialog_Load(object sender, EventArgs e)
         {
-            banner.Image = Runtime.MsiSession.GetResourceBitmap("WixUI_Bmp_Banner");
+            banner.Image = Runtime.Session.GetResourceBitmap("WixUI_Bmp_Banner");
             Text = "[ProductName] Setup";
 
             //resolve all Control.Text cases with embedded MSI properties (e.g. 'ProductName') and *.wxl file entries
