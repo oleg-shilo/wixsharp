f78b1e1a5e1e7875195a24b97207ea6fa136514c
Evgeniy Pilyaev <evgeniypilyaev@gmail.com>
2021-01-26 13:58:51 +0200

    Add OSValidation functionality to NSIS bootstrapper
    Signed-off-by: Evgeniy Pilyaev <evgeniypilyaev@gmail.com>

---
+++ Source/src/WixSharp/Nsis/WinVer/WindowsVersion.cs
@@ -0,0 +1,29 @@
+﻿namespace WixSharp.Nsis.WinVer
+{
+    /// <summary>
+    /// Represents Windows Version
+    /// </summary>
+    public struct WindowsVersion
+    {
+        /// <summary>
+        /// Creates an instance of WindowsVersion struct
+        /// </summary>
+        /// <param name="versionNumber">Represents a windows version</param>
+        /// <param name="servicePack">Represents an optional service pack (-1 = no service pack)</param>
+        public WindowsVersion(WindowsVersionNumber versionNumber, int servicePack = -1)
+        {
+            VersionNumber = versionNumber;
+            ServicePack = servicePack;
+        }
+
+        /// <summary>
+        /// Version to check
+        /// </summary>
+        public WindowsVersionNumber VersionNumber { get; }
+
+        /// <summary>
+        /// Service pack
+        /// </summary>
+        public int ServicePack { get; }
+    }
+}
