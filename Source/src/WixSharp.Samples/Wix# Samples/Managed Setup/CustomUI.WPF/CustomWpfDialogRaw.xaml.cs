268c0b65ac379cfe175542d6d88066f5d2e8edde
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-04-24 23:29:16 +1000

    - Added missing files

---
+++ Source/src/WixSharp.Samples/Wix# Samples/Managed Setup/CustomUI.WPF/CustomWpfDialogRaw.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Windows.Media.Imaging;
+using WixSharp;
+using WixSharp.UI.Forms;
+using WixSharp.UI.WPF;
+
+namespace MyProduct
+{
+    public partial class CustomDialogRawView : WpfDialog, IWpfDialog
+    {
+        public string User { get; set; } = Environment.UserName;
+
+        public CustomDialogRawView()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            CanProceedIsChecked_Click(null, null);
+        }
+
+        public void Init()
+        {
+            Banner.Source = this.ManagedFormHost?.Runtime.Session.GetResourceBitmap("WixUI_Bmp_Banner").ToImageSource();
+        }
+
+        void GoPrev_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Host?.Shell.GoPrev();
+        }
+
+        void GoNext_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Host?.Shell.GoNext();
+        }
+
+        void Cancel_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Host?.Shell.Cancel();
+        }
+
+        void CanProceedIsChecked_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GoNext.IsEnabled = (CanProceedIsChecked.IsChecked == true);
+        }
+    }
+}
