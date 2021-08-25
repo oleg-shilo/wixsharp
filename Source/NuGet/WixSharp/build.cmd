bec22d7b5bc3b3f8e9dd1dab5c3d1f23ed15fd4d
oleg.shilo <oleg.shilo@LBSMEL-1BQHTN2>
2021-08-26 08:45:03 +1000

    - Updated NuGet packages speciication

---
+++ Source/NuGet/WixSharp/build.cmd
@@ -0,0 +1,6 @@
+nuget pack WixSharp.bin.nuspec
+nuget pack WixSharp.lab.nuspec
+nuget pack WixSharp.WPF.nuspec
+nuget pack WixSharp.nuspec
+rem nuget pack WixSharp.wix.bin
+pause
--- Source/src/WixSharp.Samples/Wix# Samples/Release Folder/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Release Folder/setup.cs
@@ -27,7 +27,7 @@ class Script
                             file.OverwriteOnInstall = true;
                         }
                     },
-        new ExeFileShortcut("Uninstall My Product", "[System64Folder]msiexec.exe", "/x [ProductCode]"))); ;
+            new ExeFileShortcut("Uninstall My Product", "[System64Folder]msiexec.exe", "/x [ProductCode]")));
 
         project.GUID = new Guid("6f330b47-2577-43ad-9095-1561ba25889b");
 
