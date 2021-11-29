0ef9a962d66273c2ddc578fb58b73daf5689a1ec
oleg.shilo <oleg.shilo@gmail.com>
2021-11-30 08:32:48 +1100

    - Issue #1096: Setting RegistrySearch Id causes infinite loop

--- Source/src/WixSharp/RegistrySearch.cs
+++ Source/src/WixSharp/RegistrySearch.cs
@@ -6,7 +6,8 @@ using Microsoft.Win32;
 namespace WixSharp
 {
     /// <summary>
-    /// The value must be 'file' if the child is a FileSearch element, and must be 'directory' if child is a DirectorySearch element. This attribute's value must be one of the following:
+    /// The value must be 'file' if the child is a FileSearch element, and must be 'directory' if
+    /// child is a DirectorySearch element. This attribute's value must be one of the following:
     /// </summary>
     public enum RegistrySearchType
     {
@@ -21,13 +22,22 @@ namespace WixSharp
         file,
 
         /// <summary>
-        /// <para> Sets the raw value from the registry value. Please note that this value will contain a prefix as follows:</para>
+        /// <para>
+        /// Sets the raw value from the registry value. Please note that this value will contain a
+        /// prefix as follows:
+        /// </para>
         /// <para></para>
-        /// <para> DWORD: Starts with '#' optionally followed by '+' or '-'.</para>
-        /// <para> REG_BINARY: Starts with '#x' and the installer converts and saves each hexadecimal digit (nibble) as an ASCII character prefixed by '#x'.</para>
-        /// <para> REG_EXPAND_SZ: Starts with '#%'.</para>
-        /// <para> REG_MULTI_SZ: Starts with '[~]' and ends with '[~]'.</para>
-        /// <para> REG_SZ: No prefix, but if the first character of the registry value is '#', the installer escapes the character by prefixing it with another '#'.</para>
+        /// <para>DWORD: Starts with '#' optionally followed by '+' or '-'.</para>
+        /// <para>
+        /// REG_BINARY: Starts with '#x' and the installer converts and saves each hexadecimal digit
+        ///             (nibble) as an ASCII character prefixed by '#x'.
+        /// </para>
+        /// <para>REG_EXPAND_SZ: Starts with '#%'.</para>
+        /// <para>REG_MULTI_SZ: Starts with '[~]' and ends with '[~]'.</para>
+        /// <para>
+        /// REG_SZ: No prefix, but if the first character of the registry value is '#', the
+        ///         installer escapes the character by prefixing it with another '#'.
+        /// </para>
         /// </summary>
         raw
     }
@@ -35,18 +45,19 @@ namespace WixSharp
     /// <summary>
     /// Searches for file, directory or registry key and assigns to value of parent Property.
     /// </summary>
-    /// <example>The following sample demonstrates how to use RegisterySearch in canonical WiX RegisterySearch and FileSearch use-case.
-    /// the reg file:
+    /// <example>
+    /// The following sample demonstrates how to use RegisterySearch in canonical WiX
+    /// RegisterySearch and FileSearch use-case. the reg file:
     /// <code>
-    /// var project =
-    ///     new Project("MyProduct",
-    ///         new Dir(@"%ProgramFiles%\My Company\My Product",
-    ///         new Property("SQL_BROWSER_LOCATION",
-    ///             new RegistrySearch(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\services\SQLBrowser", "ImagePath", RegistrySearchType.file,
-    ///                 new FileSearch("sqlbrowser.exe"))),
-    ///         ...
+    ///var project =
+    ///new Project("MyProduct",
+    ///new Dir(@"%ProgramFiles%\My Company\My Product",
+    ///new Property("SQL_BROWSER_LOCATION",
+    ///new RegistrySearch(RegistryHive.LocalMachine, @"SYSTEM\CurrentControlSet\services\SQLBrowser", "ImagePath", RegistrySearchType.file,
+    ///new FileSearch("sqlbrowser.exe"))),
+    ///...
     ///
-    /// Compiler.BuildMsi(project);
+    ///Compiler.BuildMsi(project);
     /// </code>
     /// </example>
     public class RegistrySearch : GenericNestedEntity, IGenericEntity
@@ -54,14 +65,13 @@ namespace WixSharp
         /// <summary>
         /// Signature to be used for the file, directory or registry key being searched for.
         /// </summary>
-        /// <value>
-        /// The identifier.
-        /// </value>
+        /// <value>The identifier.</value>
         [Xml]
-        new public string Id { get => base.Id; set => Id = value; }
+        new public string Id { get => base.Id; set => base.Id = value; }
 
         /// <summary>
-        /// Registry value name. If this value is null, then the value from the key's unnamed or default value, if any, is retrieved.
+        /// Registry value name. If this value is null, then the value from the key's unnamed or
+        /// default value, if any, is retrieved.
         /// </summary>
         [Xml]
         new public string Name;
@@ -86,17 +96,18 @@ namespace WixSharp
         public string Key;
 
         /// <summary>
-        /// The value must be 'file' if the child is a FileSearch element, and must be 'directory' if child is a DirectorySearch element.
+        /// The value must be 'file' if the child is a FileSearch element, and must be 'directory'
+        /// if child is a DirectorySearch element.
         /// </summary>
         [Xml]
         public RegistrySearchType Type;
 
         /// <summary>
-        /// Instructs the search to look in the 64-bit registry when the value is 'true'.
-        /// When the value is 'false', the search looks in the 32-bit registry.
-        /// The default value is based on the platform set by the -arch switch to candle.exe or
-        /// the InstallerPlatform property in a .wixproj MSBuild project: For x86 and ARM,
-        /// the default value is 'false'. For x64 and IA64, the default value is 'true'.
+        /// Instructs the search to look in the 64-bit registry when the value is 'true'. When the
+        /// value is 'false', the search looks in the 32-bit registry. The default value is based on
+        /// the platform set by the -arch switch to candle.exe or the InstallerPlatform property in
+        /// a .wixproj MSBuild project: For x86 and ARM, the default value is 'false'. For x64 and
+        /// IA64, the default value is 'true'.
         /// </summary>
         public bool? Win64;
 
@@ -137,8 +148,9 @@ namespace WixSharp
         }
 
         /// <summary>
-        /// Adds itself as an XML content into the WiX source being generated from the <see cref="T:WixSharp.Project" />.
-        /// See 'Wix#/samples/Extensions' sample for the details on how to implement this interface correctly.
+        /// Adds itself as an XML content into the WiX source being generated from the <see
+        /// cref="T:WixSharp.Project"/>. See 'Wix#/samples/Extensions' sample for the details on how
+        /// to implement this interface correctly.
         /// </summary>
         /// <param name="context">The context.</param>
         public void Process(ProcessingContext context)
