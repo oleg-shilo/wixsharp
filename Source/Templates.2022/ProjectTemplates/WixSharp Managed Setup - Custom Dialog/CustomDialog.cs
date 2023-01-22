81874a7c6d7f16e595608d88d1306d07e4a6d78c
oleg.shilo <oleg.shilo@gmail.com>
2023-01-22 18:21:06 +1100

    WixSharp.VSIX: v1.17.2.0 - Issue #511: Compile error CustomDialog   Updated Project template VS extension

--- Source/Templates.2022/ProjectTemplates/WixSharp Managed Setup - Custom Dialog/CustomDialog.cs
+++ Source/Templates.2022/ProjectTemplates/WixSharp Managed Setup - Custom Dialog/CustomDialog.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using WixSharp;
 using WixSharp.UI.Forms;
 
-namespace WixSharpSetup
+namespace $safeprojectname$
 {
     public partial class CustomDialog : ManagedForm, IManagedDialog
     {
@@ -11,13 +11,13 @@ namespace WixSharpSetup
         {
             //NOTE: If this assembly is compiled for v4.0.30319 runtime, it may not be compatible with the MSI hosted CLR.
             //The incompatibility is particularly possible for the Embedded UI scenarios.
-            //The safest way to avoid the problem is to compile the assembly for v3.5 Target Framework.$safeprojectname$
+            //The safest way to avoid the problem is to compile the assembly for v3.5 Target Framework.
             InitializeComponent();
         }
 
         void dialog_Load(object sender, EventArgs e)
         {
-            banner.Image = Runtime.Session.GetResourceBitmap("WixUI_Bmp_Banner");
+            banner.Image = MsiRuntime.Session.GetEmbeddedBitmap("WixUI_Bmp_Banner");
             Text = "[ProductName] Setup";
 
             //resolve all Control.Text cases with embedded MSI properties (e.g. 'ProductName') and *.wxl file entries
