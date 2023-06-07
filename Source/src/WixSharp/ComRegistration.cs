c4f8615ce8e47c7162edb30656669d0d326f79ff
oleg.shilo <oleg.shilo@gmail.com>
2023-06-07 20:47:54 +1000

    - Fixed problem with the help builder (SHFB)

--- Source/src/WixSharp/ComRegistration.cs
+++ Source/src/WixSharp/ComRegistration.cs
@@ -246,10 +246,10 @@ namespace WixSharp
         /// most common for there to be a single value for the Context attribute. This attribute's value should be a
         /// space-delimited list congaing one or more of the following:
         /// <list type="bullet">
-        /// <item> <term>LocalServer</term>  <description>A 16-bit local server application.</description></item>
-        /// <item><term>LocalServer32</term><description>A 32-bit local server application.</description></item>
-        /// <item><term>InprocServer</term><description>A 16-bit in-process server DLL.</description></item>
-        /// <item><term>InprocServer32</term><description>A 32-bit in-process server DLL.</description></item>
+        /// <item><c>LocalServer</c>  - A 16-bit local server application.</item>
+        /// <item><c>LocalServer32</c> - A 32-bit local server application.</item>
+        /// <item><c>InprocServer</c> - A 16-bit in-process server DLL.</item>
+        /// <item><c>InprocServer32</c> - A 32-bit in-process server DLL.</item>
         /// </list>
         /// </summary>
         [Xml]
