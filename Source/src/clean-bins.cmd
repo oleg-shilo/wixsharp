8d1c819ce2ebb49e5ee4b193f601db042da61d35
oleg.shilo <oleg.shilo@gmail.com>
2023-08-12 16:30:56 +1000

    - WiX4: RC preparation

--- Source/src/clean-bins.cmd
+++ Source/src/clean-bins.cmd
@@ -2,20 +2,14 @@ echo off
 
 echo Aggregating all artifacts for the release package. Not all steps may succeed on all PCs (e.g. building chm)
 
-rd /S /Q .\..\bin\WixSharp
-
 xcopy /S /Y "WixSharp.Samples\Wix_bin" "..\bin\WixSharp\Wix_bin\"
 xcopy /S /Y "WixSharp.Samples\Wix# Samples" "..\bin\WixSharp\Samples\"
-copy /Y "WixSharp.Samples\uninstall.cmd" "..\bin\WixSharp\uninstall.cmd"
-copy /Y "WixSharp.Samples\install.cmd" "..\bin\WixSharp\install.cmd"
 copy /Y "WixSharp.Samples\cscs.exe" "..\bin\WixSharp\cscs.exe"
 copy /Y "WixSharp.Samples\nbsbuilder.exe" "..\bin\WixSharp\nbsbuilder.exe"
 copy /Y "..\license.txt" "..\bin\WixSharp\license.txt"
 copy /Y "..\readme.txt" "..\bin\WixSharp\readme.txt"
 copy /Y "WixSharp.Samples\WixSharp.Lab.dll" "..\bin\WixSharp\WixSharp.Lab.dll"
 copy /Y "WixSharp.Samples\WixSharp.Lab.xml" "..\bin\WixSharp\WixSharp.Lab.xml"
-
-.\WixSharp.Samples\cscs.exe -l .\..\bin\clean_bins.cs
 
 set msbuild=C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe
 
