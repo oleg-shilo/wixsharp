17b6254c73798eb4d9d89824b342f148f7de3d47
Oleg Shilo <oleg.shilo@gmail.com>
2023-12-14 16:55:29 +1100

    -WiX4:  #1363: Shortcut to Dir/Folder

--- Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/Shortcuts/setup.cs
@@ -2,11 +2,11 @@
 //css_ref Wix_bin\WixToolset.Dtf.WindowsInstaller.dll;
 //css_ref System.Core.dll;
 
-using Microsoft.Win32;
 using System;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml;
+using Microsoft.Win32;
 using WixSharp;
 using WixSharp.CommonTasks;
 
@@ -40,7 +40,7 @@ class Script
                            ),
 
                     new Dir(@"%ProgramMenu%\My Company\My Product",
-                        new ExeFileShortcut("Samples", "[Samples]", ""),
+                        new DirectoryShortcut("Samples", "[Samples]", ""),
                         new ExeFileShortcut("Uninstall MyApp", "[System64Folder]msiexec.exe", "/x [ProductCode]")));
 
             project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
