f98c8002cf77919f158d5aa8d6c9e32f08aa1db4
Oleg Shilo <oleg.shilo@gmail.com>
2024-02-15 14:46:41 +1100

    WiX4: Release v2.1.0.0 - #1446: Catch all exceptions in managed WinForms UI - #1444: wix.exe not found if installed as local tool - #1436: NuGet config variable "globalPackagesFolder" is ignored - #1428: [Question] SetNetPrerequirements to NET 7 or better          Updated SetNetPrerequirements to allow using new WiX4 `DotNetCompatibilityCheck` - #1436: NuGet config variable "globalPackagesFolder" is ignored - Proposal #1263: Signing using JSign or other alternatives - Added `bool None<TSource>(this IEnumerable<TSource> source, ...)` extension method - Fixed accidental renaming of `DigitalSignature1`->`DigitalSignature`

--- Source/src/WixSharp/Properties/AssemblyInfo.version.cs
+++ Source/src/WixSharp/Properties/AssemblyInfo.version.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.6.0")]
-[assembly: AssemblyFileVersion("2.0.6.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
