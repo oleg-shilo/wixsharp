f46392414c4aa2b82349cbc66da68512078e4b6d
Oleg Shilo <oleg.shilo@gmail.com>
2024-02-15 15:11:01 +1100

    WiX3-Release v1.25.0.0 - #1446: Catch all exceptions in managed WinForms UI - Added sample for #1449 - #1421 Update Text (Dialog title) on view title change (@PatrickHofman) - Proposal #1263: Signing using JSign or other alternatives

--- Source/src/WixSharp/Properties/AssemblyInfo.version.cs
+++ Source/src/WixSharp/Properties/AssemblyInfo.version.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.24.2.0")]
-[assembly: AssemblyFileVersion("1.24.2.0")]
+[assembly: AssemblyVersion("1.25.0.0")]
+[assembly: AssemblyFileVersion("1.25.0.0")]
