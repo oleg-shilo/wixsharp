7394b6a2eebbba482ed4fef2badf449625d8793b
Oleg Shilo <oleg.shilo@gmail.com>
2024-02-16 18:33:07 +1100

    - WiX3: house-keeping

--- Source/src/build.cmd
+++ Source/src/build.cmd
@@ -1,5 +1,10 @@
 echo off
 
+rem cleaning WPF bin folders (MSBuild fails to do it)
+
+rd /Q /S .\WixSharp.UI.WPF\obj
+rd /Q /S .\WixSharp.UI.WPF\bin
+
 set msbuild=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
 
 "%msbuild%" /nologo /verbosity:minimal /t:Clean,Restore,Build /p:Configuration=Release /p:Platform="Any CPU" WixSharp.Suite.sln
