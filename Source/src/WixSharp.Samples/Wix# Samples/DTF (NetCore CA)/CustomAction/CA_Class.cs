e8ce6a8eea04b05b917d86da552c8788b1c1e593
Oleg Shilo <oleg.shilo@gmail.com>
2024-02-16 23:12:52 +1100

    - WiX4: NETCore CA sample

--- Source/src/WixSharp.Samples/Wix# Samples/DTF (NetCore CA)/CustomAction/CA_Class.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/DTF (NetCore CA)/CustomAction/CA_Class.cs
@@ -6,16 +6,19 @@ namespace CustomAction;
 public class Class1
 {
     [DllImport("user32.dll")]
-    public static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+    static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
 
     [UnmanagedCallersOnly(EntryPoint = "CustomActionCore")]
     public static uint CustomActionCore(IntPtr handle)
     {
         using Session session = Session.FromHandle(handle, false);
 
-        MessageBox(IntPtr.Zero, "Hello from .NET Core!", "WixSharp", 0);
+        MessageBox(GetForegroundWindow(), "Hello from .NET Core!", "WixSharp", 0);
         session.Log("CustomActionCore invoked");
 
-        return 0;
+        return (uint)ActionResult.Success;
     }
 }
