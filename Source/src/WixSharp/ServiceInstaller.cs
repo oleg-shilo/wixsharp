fb452ff473476460be19ab5e27a46b6c3088326a
Oleg Shilo <oleg.shilo@gmail.com>
2024-02-29 11:56:01 +1100

    WiX4: Issue #1460: Add -sw1026 to default Wix4 argument options?

--- Source/src/WixSharp/ServiceInstaller.cs
+++ Source/src/WixSharp/ServiceInstaller.cs
@@ -358,6 +358,14 @@ namespace WixSharp
         /// <param name="context">The context.</param>
         public void Process(ProcessingContext context)
         {
+            if (!Compiler.WixOptions.Contains("-sw1149"))
+                Compiler.OutputWriteLine("WARNING: You used one of the ServiceInstaller properties (DelayedAutoStart, " +
+                         "PreShutdownDelay, ServiceSid) that WiX can only support via its old implementation of `ServiceConfig` " +
+                         "element. WiX compiler will generate the `warning WIX1149` about this element and suggest using a newer " +
+                         "WixUtilExtension ServiceConfig element instead.\n" +
+                         "However since the new element does not support DelayedAutoStart, PreShutdownDelay, ServiceSid you need to " +
+                         "asses the WiX warning and decide if you want keep or drop the use of these attributes.");
+
             if (RequiresConfig())
             {
                 Config = new ServiceConfig
