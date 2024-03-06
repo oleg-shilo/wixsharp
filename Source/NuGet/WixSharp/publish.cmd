24713a879286b345c22198e654189f8b17dc1c51
Oleg Shilo <oleg.shilo@gmail.com>
2024-03-06 21:58:40 +1100

    NetCore: WixSharp VS Templates v2.2.6 published

--- Source/NuGet/WixSharp/publish.cmd
+++ Source/NuGet/WixSharp/publish.cmd
@@ -1,8 +1,8 @@
-dotnet nuget push WixSharp.Msi.Core.2.1.2.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
-dotnet nuget push WixSharp.Core.2.1.2.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
+dotnet nuget push WixSharp.Msi.Core.2.1.3.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
+dotnet nuget push WixSharp.Core.2.1.3.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push WixSharp.Msi.Core.2.1.2.snupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
-dotnet nuget push WixSharp.Core.2.1.2.snupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
+dotnet nuget push WixSharp.Msi.Core.2.1.3.snupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
+dotnet nuget push WixSharp.Core.2.1.3.snupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
 
 dotnet nuget push WixSharp_wix4.bin.2.1.2.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
 dotnet nuget push WixSharp_wix4.2.1.2.nupkg -k %nugetkey% -s https://api.nuget.org/v3/index.json
