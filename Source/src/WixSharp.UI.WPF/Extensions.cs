c377aa872d0d46deb5f3306d7af593ada1397786
Oleg Shilo <oleg.shilo@gmail.com>
2024-08-17 20:12:17 +1000

    wix3: #1604: Some strings in Dialogs from .wxl are not replaced in Custom WPF UI

--- Source/src/WixSharp.UI.WPF/Extensions.cs
+++ Source/src/WixSharp.UI.WPF/Extensions.cs
@@ -236,6 +236,17 @@ namespace WixSharp.UI.WPF
     /// <seealso cref="WixSharp.IManagedDialog" />
     public class WpfDialog : UserControl, IManagedDialog
     {
+        bool isLocalizationScheduled = false;
+
+        internal void ScheduleOnLoadLocalization()
+        {
+            if (!isLocalizationScheduled)
+            {
+                Loaded += (s, e) => Localize();
+                isLocalizationScheduled = true;
+            }
+        }
+
         string dialogTitle;
 
         /// <summary>
@@ -367,7 +378,7 @@ namespace WixSharp.UI.WPF
 
                 if (content is WpfDialog wpfDialog)
                 {
-                    wpfDialog.Localize();
+                    wpfDialog.ScheduleOnLoadLocalization();
                     this.Text = wpfDialog.DialogTitle;
                 }
                 this.Localize();
