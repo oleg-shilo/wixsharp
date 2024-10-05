592421986236b40a76e18ba296621b63afd0eca7
Oleg Shilo <oleg.shilo@gmail.com>
2024-10-05 22:22:01 +1000

    - Wix3: CI refinement

--- Source/src/build.cmd
+++ Source/src/build.cmd
@@ -5,6 +5,9 @@ rem cleaning WPF bin folders (MSBuild fails to do it)
 rd /Q /S .\WixSharp.UI.WPF\obj
 rd /Q /S .\WixSharp.UI.WPF\bin
 
+rem set EnableNuGetPackageRestore=true
+nuget restore WixSharp.Suite.sln
+
 set msbuild=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe
 
 "%msbuild%" /nologo /verbosity:minimal /t:Clean,Restore,Build /p:Configuration=Release /p:Platform="Any CPU" WixSharp.Suite.sln
