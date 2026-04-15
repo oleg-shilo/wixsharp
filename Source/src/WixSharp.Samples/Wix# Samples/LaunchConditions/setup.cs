1330075113437ac2588ccfb8cca4d80e0cc1cfc5
Oleg Shilo <oleg.shilo@gmail.com>
2026-04-15 17:51:46 +1000

    LaunchCondition sample update

--- Source/src/WixSharp.Samples/Wix# Samples/LaunchConditions/setup.cs
+++ Source/src/WixSharp.Samples/Wix# Samples/LaunchConditions/setup.cs
@@ -2,8 +2,10 @@
 //css_ref Wix_bin\WixToolset.Dtf.WindowsInstaller.dll;
 //css_ref System.Core.dll;
 using System;
+using System.Windows.Forms;
 using WixSharp;
 using WixSharp.CommonTasks;
+using WixToolset.Dtf.WindowsInstaller;
 
 class Script
 {
@@ -41,6 +43,36 @@ class Script
         //project.SetNetFxPrerequisite(Condition.Net20_Installed, "Please install .NET 2.0 first.");
         //project.SetNetFxPrerequisite("NETFRAMEWORK45 >= '#378389'", "Please install .Net 4.5 First");
         //project.SetNetFxPrerequisite("NETFRAMEWORK30_SP_LEVEL and NOT NETFRAMEWORK30_SP_LEVEL='#0'", "Please install .NET 2.0 Service Pack first.");
+
+        Compiler.BuildMsi(project);
+    }
+
+    static public void CheckDotNetWithCustomActionTask()
+    {
+        var project = new ManagedProject("Setup",
+            new Dir(@"%ProgramFiles%\My Company\My Product",
+                new File(@"Files\MyApp.exe")));
+
+        project.Load += e =>
+        {
+            if (e.IsInstalling)
+            {
+                // hard-codded but needs to be replaced with the real checking of registry
+                // [localmachine]\Software\Microsoft\NET Framework Setup\NDP\v2.0.50727
+                bool isCorrectNetFrameworkInstalled = false;
+
+                if (!isCorrectNetFrameworkInstalled)
+                {
+                    string message = "Please install .NET 2.0 first.";
+                    e.Session.Log(message);
+
+                    if (e.UILevel > 4)
+                        MessageBox.Show(message, e.ProductName);
+
+                    e.Result = ActionResult.Failure;
+                }
+            }
+        };
 
         Compiler.BuildMsi(project);
     }
